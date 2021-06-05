f95797e09c4bc32baa9216c23789e121319e5d3b
DelaneKay <kudakwashedelanesibanda@gmail.com>
2021-06-05 16:17:40 +0200

    sliding menu

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './components/header/Header_section'
+
+
+
+function App() {
+  return (
+    <>
+        <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header_section.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Search, List } from 'react-bootstrap-icons'
+import { Button } from 'react-bootstrap'
+
+const Header_section = () => {
+    function openMenu(){
+        document.getElementById('side-menu').style.marginRight = '0px';
+        document.body.style.marginRight = '325px';
+    }
+    
+    function closeMenu(){
+        document.getElementById('side-menu').style.marginRight = '-325px';
+        document.body.style.marginRight = '0';
+    }
+
+    return (
+        <header className='home-section' id='main'>
+            <section id='header'>
+                <nav className='navbar py-4'>
+                    <div className="container">
+                        <Search id='search'/>
+                        <a href="#" className="nav-brand mx-auto">TechLAB</a>
+                        <List onClick={()=>openMenu()} id= 'list'/>
+                    </div>
+                    <div id="side-menu">
+                        <ul>
+                            <li href=''>Artificial Intelligence</li>
+                            <li href=''>Gadgets</li>
+                            <li href=''>Robots</li>
+                            <li href='' id='iot'>IoT</li>
+                        </ul>
+                    </div>
+                </nav>
+            </section>
+            <div className="jumbotron text-center">
+                <div className="intro">
+                    <h1>The Future Is finally Here!</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style component for calling
+                        extra attention to featured content or information.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header_section
+
